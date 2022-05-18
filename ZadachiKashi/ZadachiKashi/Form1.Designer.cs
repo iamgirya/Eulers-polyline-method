43cd328edc51807a1c63152911aa926d01318f91
iamgirya <iamgirya@yandex.ru>
2022-05-18 15:35:17 +0300

    Копирование старого проекта

---
+++ ZadachiKashi/ZadachiKashi/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace ZadachiKashi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(1067, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(1067, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(75, 45);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Xo:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(1067, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(75, 45);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Yo:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(1067, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 45);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1067, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 60);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(1067, 216);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(75, 45);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "E:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(1067, 267);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(75, 45);
+            this.textBox6.TabIndex = 6;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(13, 12);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1047, 520);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 545);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ ZadachiKashi/ZadachiKashi/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace ZadachiKashi
+{
+    //Метод Рунге-Кутта
+    public partial class Form1 : Form
+    {
+        double xo, yo, e;
+        static public double f(double x, double y)
+        {
+            return 2*x +y;
+            //return Math.Sin(x);
+        }
+        static public double realF(double x)
+        {
+            return -2 * (x + 1) + Math.Exp(x)*4;
+            //return -Math.Cos(x);
+        }
+
+        private void Clear(ZedGraphControl Zed_GraphControl)
+        {
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            zedGraphControl1.GraphPane.GraphObjList.Clear();
+
+            zedGraphControl1.GraphPane.XAxis.Type = AxisType.Linear;
+            zedGraphControl1.GraphPane.XAxis.Scale.TextLabels = null;
+            zedGraphControl1.GraphPane.XAxis.MajorGrid.IsVisible = false;
+            zedGraphControl1.GraphPane.YAxis.MajorGrid.IsVisible = false;
+            zedGraphControl1.GraphPane.YAxis.MinorGrid.IsVisible = false;
+            zedGraphControl1.GraphPane.XAxis.MinorGrid.IsVisible = false;
+            zedGraphControl1.RestoreScale(zedGraphControl1.GraphPane);
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        public void build(ZedGraphControl Zed_GraphControl)
+        {
+
+            PointPairList list = new PointPairList();
+            PointPairList reallist = new PointPairList();
+
+            double h, x=xo, y=yo;
+            long n = (long)((50-xo)/e) + 1;
+            h = (50 - xo) / n;
+
+            while (x < 50+h/2)
+            {
+                if (x > 0)
+                {
+                    list.Add(x, y);
+                    reallist.Add(x, realF(x));
+                }
+
+                double k1 = f(x, y);
+                double k2 = f(x + h/2, y+k1*h/2);
+                double k3 = f(x + h / 2, y + k2 * h / 2);
+                double k4 = f(x + h , y + k3 * h);
+
+                y = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+                x = x + h;
+            }
+
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+            LineItem myCircle = my_Pane.AddCurve("Pribl", list, Color.Blue, SymbolType.Circle);
+            LineItem myCircle2 = my_Pane.AddCurve("Func", reallist, Color.Green, SymbolType.Square);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs ee)
+        {
+            if (textBox1.Text != "" && textBox4.Text != "" && textBox6.Text != "")
+            {
+                try
+                {
+                    xo = Convert.ToDouble(textBox1.Text);
+                    yo = Convert.ToDouble(textBox4.Text);
+                    e = Convert.ToDouble(textBox6.Text);
+                }
+                catch { return; }
+                Clear(zedGraphControl1);
+                build(zedGraphControl1);
+                
+            }
+        }
+    }
+}
